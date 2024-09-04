5329ebac7180894c03b82a41d198fb450a11b16c
Vitaliy Nekrasov <vitaliynekrasov1@gmail.com>
2024-09-04 15:31:06 +0300

    Restore api functions

--- src/services/movies-api.ts
+++ src/services/movies-api.ts
@@ -1,12 +1,19 @@
 import axios, { AxiosResponse } from "axios";
 import { RootInterface, Result } from "../models/models";
 
-const API_KEY = "1c308592d719c7668d47adc63622f209";
+const API_KEY = "630bad0f090cd848fcec8ebe6943deeb";
 const baseURL = "https://api.themoviedb.org/3/";
 
 export async function getTrendingMovies(page: number): Promise<Result[]> {
+  const options = {
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzBiYWQwZjA5MGNkODQ4ZmNlYzhlYmU2OTQzZGVlYiIsIm5iZiI6MTcyNTQ1MTYzNi45NTAxLCJzdWIiOiI2NmQ4NGJiMzMyMjE1NmQ0MWE5ZDJjNGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.iMSHIiBP9MxkTrzAeqVevxMoI7e7_yavJ1tyomH0Hjo",
+    },
+  };
   const resp: AxiosResponse<RootInterface> = await axios.get(
-    `${baseURL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`
+    `${baseURL}/movie/popular?&language=en-US&page=${page}`, options
   );
   const result: Result[] = await resp.data.results;
   return result;
